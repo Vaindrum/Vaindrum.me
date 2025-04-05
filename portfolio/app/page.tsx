8f6063de54da1364fbda7f107674136072c0c97e
Vaibhav Raj <whybebhav@gmail.com>
2025-04-05 12:11:51 +0530

    Approach Section

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -5,6 +5,7 @@ import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
 export default function Home() {
   return (
     <main className="relative bg-black-100 text-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
@@ -16,6 +17,7 @@ export default function Home() {
       <RecentProjects />
       <Clients />
       <Experience />
+      <Approach />
       </div>
     </main>
   );
